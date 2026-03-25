969b0077ee753d2a3fadc497be4c2be311979523
Mubarek Idris <mubarekimmortal1@gmail.com>
2026-03-25 18:50:12 -0500

    Fix persistence crash when orders.dat is missing (handle first run case)

--- src/main/java/edu/metrostate/warehouseapp/HelloApplication.java
+++ src/main/java/edu/metrostate/warehouseapp/HelloApplication.java
@@ -4,12 +4,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import model.OrderSystem;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        // Create the system so persistence/load logic can run
+        OrderSystem orderSystem = new OrderSystem();
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
