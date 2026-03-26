0624c2c3205805f01bc3f3af7e12339e703e723f
Mubarek Idris <mubarekimmortal1@gmail.com>
2026-03-25 20:25:38 -0500

    Ready To run

--- src/main/java/edu/metrostate/warehouseapp/HelloApplication.java
+++ src/main/java/edu/metrostate/warehouseapp/HelloApplication.java
@@ -4,15 +4,30 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import model.OrderSystem;
+import model.Item;
+import model.PickupOrder;
+import model.ShipOrder;
+import model.DirectDeliveryOrder;
+import model.WarehouseManager;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        // Create the system so persistence/load logic can run
-        OrderSystem orderSystem = new OrderSystem();
+        WarehouseManager manager = WarehouseManager.getInstance();
+
+        ShipOrder order1 = new ShipOrder("1001", System.currentTimeMillis(), "WallyWorld");
+        order1.addItem(new Item("Test Item A", 2, 50.0));
+        manager.getWarehouse("Warehouse_A").addOrder(order1);
+
+        PickupOrder order2 = new PickupOrder("1002", System.currentTimeMillis(), "Bullseye");
+        order2.addItem(new Item("Test Item B", 1, 25.0));
+        manager.getWarehouse("Warehouse_B").addOrder(order2);
+
+        DirectDeliveryOrder order3 = new DirectDeliveryOrder("1003", System.currentTimeMillis(), "WallyWorld");
+        order3.addItem(new Item("Test Item C", 3, 15.0));
+        manager.getWarehouse("Warehouse_C").addOrder(order3);
 
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
